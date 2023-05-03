531686ed46c6756db1f36c8678c6de0af3bcb5f7
Peter Ajala <ajalapeter1@gmail.com>
2023-05-03 14:13:16 -0700

    worked on code indentation

--- User/registration.js
+++ User/registration.js
@@ -1,7 +1,8 @@
+// global array for user data
 var userArr = [];
 
 
-
+// FUNCTION TO GET EXISTING USERS
 const getExistingUsers = () => {
     let localUser = localStorage.getItem("userData");
     if(localUser){
@@ -12,9 +13,11 @@ const getExistingUsers = () => {
     signUp();
 }
 
+// Function to get current user from local storage
 const CurrentUser = localStorage.getItem('CurrentUser');
 curren = JSON.parse(CurrentUser);
 
+// FUNCTION FOR SIGN UP BUTTON
 const signUp = e => {
     let firstname = document.getElementById('firstname').value;
     let lastname = document.getElementById('lastname').value;
@@ -43,104 +46,23 @@ const signUp = e => {
     e.preventDefault();
 }
 
-
-
-
+// FUNCTION FOR SIGN IN BUTTON
 const signIn = e => {
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
     let userName = document.getElementById('username').value;
 
-    
     let userData = JSON.parse(localStorage.getItem('userData')) || [];
     
-
-
     let existingUser = userData.length && 
     JSON.parse(localStorage.getItem('userData')).some(data => data.email.toLowerCase() == email && data.password.toLowerCase() == password && data.username.toLowerCase() == userName);
     if(!existingUser){
         alert("Incorrect login details");
     }
     else{
-        // localStorage.setItem("currentuser", JSON.stringify(existingUser));
         localStorage.setItem('CurrentUser', JSON.stringify(userName));
-        
         window.location.href = "userpage.html";    
     }
-
     e.preventDefault();
-  }
+}
 
-//   let currentuser = JSON.parse(localStorage.getItem('currentUser'));
-
-//   // Function to get the current user's email from local storage
-// function getCurrentUser() {
-//     return localStorage.getItem("currentUser");
-// }
-
-// // Function to set the current user's email in local storage
-// function setCurrentUser(email) {
-//     localStorage.setItem("currentUser", email);
-// }
-
-// // Function to display the current user's profile page
-// function showProfilePage() {
-//     // Get the current user's email
-//     currentuser = getCurrentUser();
-// }
-
-
-
-
-  
-//     // Set up user credentials
-// const users = [
-//     { username: "user1", password: "password1", profilePage: "profile1.html" },
-//     { username: "user2", password: "password2", profilePage: "profile2.html" },
-//     { username: "user3", password: "password3", profilePage: "profile3.html" }
-//   ];
-  
-//   // Function to validate user credentials
-//   function validateUser(username, password) {
-//     for (let user of users) {
-//       if (user.username === username && user.password === password) {
-//         return user;
-//       }
-//     }
-//     return null;
-//   }
-  
-//   // Function to handle form submission
-//   function handleFormSubmit(event) {
-//     event.preventDefault();
-    
-//     // Get form input values
-//     const username = document.getElementById("username").value;
-//     const password = document.getElementById("password").value;
-    
-//     // Validate user credentials
-//     const user = validateUser(username, password);
-    
-//     if (user) {
-//       // Save user credentials to local storage
-//       localStorage.setItem("currentUser", JSON.stringify(user));
-      
-//       // Redirect to user's profile page
-//       window.location.href = user.profilePage;
-//     } else {
-//       alert("Invalid username or password. Please try again.");
-//     }
-//   }
-  
-//   // Check if user is already logged in
-//   const currentUser = JSON.parse(localStorage.getItem("currentUser"));
-  
-//   if (currentUser) {
-//     // Redirect to user's profile page
-//     window.location.href = currentUser.profilePage;
-//   }
-  
-//   // Attach form submission event listener
-//   const form = document.getElementById("login-form");
-//   form.addEventListener("submit", handleFormSubmit);
-  
--- User/userpage.js
+++ User/userpage.js
@@ -1,114 +1,113 @@
-const storedObjectString = localStorage.getItem('CurrentUser');
+// const storedObjectString = localStorage.getItem('CurrentUser');
 
-// Convert the string back to an object
-const storedObject = JSON.parse(storedObjectString);
+// // Convert the string back to an object
+// const storedObject = JSON.parse(storedObjectString);
 
 // document.getElementById('usern').innerHTML = storedObject;
 
-        var noteArr = [];
-        var isEditing = false;
-        var editingIndex;
+// GLOBAL FUNCTIONS
+    var noteArr = [];
+    var isEditing = false;
+    var editingIndex;
 
-       
-
-        
-        function getExistingNotes(){
-            let localNote = localStorage.getItem("notes");
+    
+    // FUNCTION TO GET EXISTING NOTES FROM LOCAL STORGAE
+    function getExistingNotes(){
+        let localNote = localStorage.getItem("notes");
             if(localNote){
                 noteArr = JSON.parse(localNote);
             }else{
                 noteArr = [];
             }
-            
             displayNote(); 
-
         }
         
+    // FUNCTION FOR ADD NOTE BUTTON
+    function addNote(){
+        if(isEditing){
+            let Title = document.getElementById('titleInput').value;
+            let Note = document.getElementById('noteContentInput').value;
+            let noteObj = {title:Title, note:Note, date: new Date().toUTCString()}
 
-        function addNote(){
-            if(isEditing){
-                let Title = document.getElementById('titleInput').value;
-                let Note = document.getElementById('noteContentInput').value;
-                let noteObj = {title:Title, note:Note, date: new Date().toUTCString()}
-
-                noteArr[editingIndex] = noteObj;
-
-                document.getElementById('titleInput').value = "";
-                document.getElementById('noteContentInput').value = "";
-                
-                updateLocalStorage();
-                displayNote();
-                displayRecentNote();
-            }else{
-                let Title = document.getElementById('titleInput').value;
-                let Note = document.getElementById('noteContentInput').value;
-                let noteObj = {title:Title, note:Note, date: new Date().toUTCString()};
-                noteArr.unshift(noteObj);
+            noteArr[editingIndex] = noteObj;
 // command to clear data from input bar after it has been added to the note book. 
-                document.getElementById('titleInput').value = "";
-                document.getElementById('noteContentInput').value = "";
-            
-                updateLocalStorage();
-                displayNote();
-                displayRecentNote();
+            document.getElementById('titleInput').value = "";
+            document.getElementById('noteContentInput').value = "";
                 
-            }
+            updateLocalStorage();
+            displayNote();
+            displayRecentNote();
+        }else{
+            let Title = document.getElementById('titleInput').value;
+            let Note = document.getElementById('noteContentInput').value;
+            let noteObj = {title:Title, note:Note, date: new Date().toUTCString()};
+            noteArr.unshift(noteObj);
+// command to clear data from input bar after it has been added to the note book. 
+            document.getElementById('titleInput').value = "";
+            document.getElementById('noteContentInput').value = "";
             
-           
+            updateLocalStorage();
+            displayNote();
+            displayRecentNote();     
+            }    
         }
 
-        function updateLocalStorage(){
-            localStorage.setItem ("notes", JSON.stringify(noteArr));
+    // FUNCTION TO UPDATE LOCAL STORAGE
+    function updateLocalStorage(){
+        localStorage.setItem ("notes", JSON.stringify(noteArr));
         }
 
-        function displayNote(){
-            let content = "";
-            let i;
-            for ( i = 0; i < noteArr.length; i++) {
+    //FUNCTION TO LOOP THE CONTENT SUPPLIED AND DISPLAY IT ON THE USERPAGE 
+    function displayNote(){
+        let content = "";
+        let i;
+        for ( i = 0; i < noteArr.length; i++) {
                 
-                    content +=`<div style="border-bottom:1px solid black;"> <p class="fw-semibold mt-3 fs-4">${noteArr[i].title}</p> <p class="text-primary">${noteArr[i].date}</p> <p>${noteArr[i].note}</p> <button class="mb-3 me-2 border-0 bg-danger text-white rounded-1 px-3 py-1" onclick="deleteNote(${i})">Delete</button> <button class="mb-3 border-0 bg-success text-white rounded-1 px-3 py-1" onclick="editNote(${i})">Edit</button></div>`;
-                  
-            }
+            content +=`<div style="border-bottom:1px solid black;"> <p class="fw-semibold mt-3 fs-4">${noteArr[i].title}</p> <p class="text-primary">${noteArr[i].date}</p> <p>${noteArr[i].note}</p> <button class="mb-3 me-2 border-0 bg-danger text-white rounded-1 px-3 py-1" onclick="deleteNote(${i})">Delete</button> <button class="mb-3 border-0 bg-success text-white rounded-1 px-3 py-1" onclick="editNote(${i})">Edit</button></div>`;
+        }
+
             document.getElementById('showBox').innerHTML = content;
             document.getElementById('addNoteBtn').innerHTML = 'Add Note'
             isEditing = false;
+    }
+
+    // FUNCTION TO DISPLAY RECENT NOTES
+    function displayRecentNote(){
+        let content = "";
+        let i;
+        for ( i = 0; i < 5; i++) {
+                
+            content +=`<div style="border-bottom:1px solid black;"> <p class="fw-semibold mt-3 fs-4">${noteArr[i].title}</p> <p class="text-primary">${noteArr[i].date}</p> <p>${noteArr[i].note}</p></div>`;
         }
 
-        function displayRecentNote(){
-            let content = "";
-            let i;
-            for ( i = 0; i < 5; i++) {
-                
-                    content +=`<div style="border-bottom:1px solid black;"> <p class="fw-semibold mt-3 fs-4">${noteArr[i].title}</p> <p class="text-primary">${noteArr[i].date}</p> <p>${noteArr[i].note}</p></div>`;
-                  
-            }
             document.getElementById('showRecentBox').innerHTML = content;
             document.getElementById('addNoteBtn').innerHTML = 'Add Note'
             isEditing = false;
-        }
+    }
 
-       
-        function deleteNote(i){
-            if(confirm ("Click OK to delete note!!!")) {
-                noteArr.splice(i,1);
+    
+    // FUNTION FOR DELETE BUTTON
+    function deleteNote(i){
+        if(confirm ("Click OK to delete note!!!")) {
+            noteArr.splice(i,1);
             
-                updateLocalStorage();
-                displayNote();
-                displayRecentNote();
+            updateLocalStorage();
+            displayNote();
+            displayRecentNote();
             }   
         }
 
+    // FUNCTION FOR EDIT BUTTON
+    function editNote(i){
+        if(confirm("Click OK to update your note")) {
+            editingIndex = i;
 
-        function editNote(i){
-            if(confirm("Click OK to update your note")) {
-                editingIndex = i;
+            let note = noteArr[i];
+            titleInput.value = note.title;
+            noteContentInput.value = note.note;
 
-                let note = noteArr[i];
-                titleInput.value = note.title;
-                noteContentInput.value = note.note;
-
-                document.getElementById('addNoteBtn').innerHTML = 'Update Note'
-                isEditing = true;
+            document.getElementById('addNoteBtn').innerHTML = 'Update Note'
+            isEditing = true;
             }
         }
         
